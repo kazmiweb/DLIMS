8cc2375b6c39a10da8a1f6938c3ff822a5ff069b
kazmiweb <zeezain444@gmail.com>
2024-04-14 04:31:42 +0500

    Cloned old repository

--- kazmi/urls.py
+++ kazmi/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls, name='admins'),
     path('license/', include('verifyLic.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
--- verifyLic/urls.py
+++ verifyLic/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('verify/save/', views.save_license, name='save_license'),
+    path('save/', views.save_license, name='save_license'),
     path('verify/', views.verify_license, name='verify_license'),
     # path('result/', views.verification_done, name='verification_done'),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
